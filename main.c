c0fde550be32d188db89acb9139dabe292173cf8
Abrahamlangat2021 <92026591+Abrahamlangat2021@users.noreply.github.com>
2021-10-23 15:25:18 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+
+{
+    int choice;
+    printf("LIBRARY MANAGEMENT SYSTEM\n");
+     printf("MAIN MENU\n");
+
+     printf("\n\n\n\t\t\t1.Add Books");
+        printf("\n\t\t\t2.Search Books");
+        printf("\n\t\t\t3.View Books");
+        printf("\n\t\t\t4.Delete Book");
+        printf("\n\t\t\t5.Update Password");
+        printf("\n\t\t\t0.Exit");
+        printf("\n\n\n\t\t\tEnter choice => ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+
+            printf("\n\n\n\t\t\t\tThank you!!!\n\n\n\n\n");
+            exit(1);
+
+
+        }
+
+    printf("welcome");
+    return 0;
+}
